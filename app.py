8096b1b61ad20769436e456d8ca2825449c2e4d3
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-11-27 11:59:09 +0100

    season

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ df.fillna(df.mean(), inplace=True)
 # Selezioniamo solo le colonne rilevanti
 selected_columns = [
     'hotel', 'meal', 'arrival_date_year', 'arrival_date_month', 'is_high_season',
-    'is_canceled', 'previous_cancellations', 'previous_bookings_not_canceled',
+    'is_canceled', 'season',
     'adults', 'children', 'babies', 'total_guests', 'adr', 'is_repeated_guest'
 ]
 
