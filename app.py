6e3218f308a5a433148a3a0b59a23d7fda784b14
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-11-28 09:35:23 +0100

    random forest

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, r2_score
 import plotly.express as px
@@ -32,8 +32,8 @@ y.fillna(y.mean(), inplace=True)
 # Dividiamo i dati in set di addestramento e test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Addestramento del modello di regressione lineare
-model = LinearRegression()
+# Addestramento del modello Random Forest Regressor
+model = RandomForestRegressor(n_estimators=100, random_state=0)
 model.fit(X_train, y_train)
 
 # Streamlit UI
